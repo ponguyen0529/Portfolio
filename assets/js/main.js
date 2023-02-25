257ad5affa9a852a44772853dca7da2cf22e0ade
Po Nguyen <ponguyen0529@gmail.com>
2023-02-24 23:59:40 -0500

    Testimonial Sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,7 +97,25 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+let swiper = new Swiper(".testimonials__container", {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
 
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+        },
+    },
+});
 
 /*=============== INPUT ANIMATION ===============*/
 
